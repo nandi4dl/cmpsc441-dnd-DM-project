e95cbbd245bfd73ba849c131b16755defdd4666e
Nandi <148650143+nandi4dl@users.noreply.github.com>
2025-03-25 11:06:03 -0400

    Initial playerclass and function file

---
+++ LibraryTest.py
@@ -0,0 +1,21 @@
+from dnd_character import *
+import json
+def playerCharCreate(name,age,level,gender,description,background,playerclass):
+    newChar = Character(
+        name=name,
+        age=age,
+        level=level,
+        gender=gender,
+        description=description,
+        background=background,
+        classs=CLASSES[playerclass.lower()])
+    return newChar
+
+print(playerCharCreate(name="Thor Odinson",
+    age="34",
+    level=1,
+    gender="Male",
+    description="Looks like a pirate angel",
+    background="Born on Asgard, God of Thunder",
+    playerclass="Fighter"))
+
